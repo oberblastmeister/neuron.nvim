34e0f37e170d548c5e45b5b216921bb4bc2d9362
Brian Shu <littlebubu.shu@gmail.com>
2021-01-02 16:50:02 -0500

    started branch

--- lua/neuron.lua
+++ lua/neuron.lua
@@ -240,22 +240,72 @@ do
     end
   end
 
-  local lock = false
+  List = {}
+  function List.new ()
+    return {first = 0, last = -1}
+  end
+
+  function List.pushleft (list, value)
+    local first = list.first - 1
+    list.first = first
+    list[first] = value
+  end
+
+  function List.pushright (list, value)
+    local last = list.last + 1
+    list.last = last
+    list[last] = value
+  end
+
+  function List.popleft (list)
+    local first = list.first
+    if first > list.last then return nil end
+    local value = list[first]
+    list[first] = nil        -- to allow garbage collection
+    list.first = first + 1
+    return value
+  end
+
+  function List.is_empty(list)
+    return list.first > list.last
+  end
+
+  function List.popright (list)
+    local last = list.last
+    if list.first > last then return nil end
+    local value = list[last]
+    list[last] = nil         -- to allow garbage collection
+    list.last = last - 1
+    return value
+  end
+
+  function List.len(list)
+    return list.last - list.first
+  end
+
+  local queue = List.new()
+  local MAX_QUEUED = 2
 
   function M.attach_buffer_fast()
     api.nvim_buf_attach(0, true, {
         on_lines = vim.schedule_wrap(function(...)
-          if lock == false then
-            local params = {...}
-            vim.defer_fn(function()
-              on_lines(params)
-
-              -- done
-              lock = false
-            end, 100)
-
-            lock = true -- lock it for better perf
+          if List.len(queue) > MAX_QUEUED then
+            List.popright(queue)
           end
+
+          List.pushleft(queue, {...})
+
+          -- print(string.format("first: %s, last: %s", queue.first, queue.last))
+
+          vim.defer_fn(function()
+
+            local popped = List.popright(queue)
+            if popped == nil then
+              return
+            end
+
+            on_lines(popped)
+          end, 100)
         end)
       })
   end
