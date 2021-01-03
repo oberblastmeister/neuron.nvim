32c9e384cdd473dcf16836f15d2bcac790655188
Brian Shu <littlebubu.shu@gmail.com>
2021-01-02 19:10:15 -0500

    started branch

--- lua/neuron.lua
+++ lua/neuron.lua
@@ -240,91 +240,19 @@ do
     end
   end
 
-  List = {}
-  function List.new (max)
-    return {first = 0, last = -1, max = max}
-  end
-
-  function List.pushleft (list, value)
-    if List.len_too_large(list) then
-      return true
-    end
-
-    local first = list.first - 1
-    list.first = first
-    list[first] = value
-
-    return false
-  end
-
-  function List.pushright (list, value)
-    if List.len_too_large(list) then
-      return true
-    end
-
-    local last = list.last + 1
-    list.last = last
-    list[last] = value
-
-    return false
-  end
-
-  function List.len_too_large(list)
-    if list.max then
-      if List.len(list) > list.max then
-        return true
-      end
-    end
-
-    return false
-  end
-
-  function List.popleft (list)
-    local first = list.first
-    if first > list.last then return nil end
-    local value = list[first]
-    list[first] = nil        -- to allow garbage collection
-    list.first = first + 1
-    return value
-  end
-
-  function List.is_empty(list)
-    return list.first > list.last
-  end
-
-  function List.popright (list)
-    local last = list.last
-    if list.first > last then return nil end
-    local value = list[last]
-    list[last] = nil         -- to allow garbage collection
-    list.last = last - 1
-    return value
-  end
-
-  function List.len(list)
-    return list.last - list.first
-  end
-
-  local queue = List.new(1)
+  local task
 
   function M.attach_buffer_fast()
     api.nvim_buf_attach(0, true, {
         on_lines = vim.schedule_wrap(function(...)
-          local params = {...}
-          local full = List.pushleft(queue, params)
-          if full then
-            List.popright(queue)
-            List.pushleft(queue, params)
-          end
+          local empty = task == nil
 
-          if not full then
+          task = {...}
+
+          if empty then
             vim.defer_fn(function()
-              local popped = List.popright(queue)
-              if popped == nil then
-                return
-              end
-
-              on_lines(popped)
+              on_lines(task)
+              task = nil
             end, 100)
           end
         end)
