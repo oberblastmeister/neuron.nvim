5b44860bfbc57af5759ac4de7c75eef2cabce794
Brian Shu <littlebubu.shu@gmail.com>
2021-03-21 20:56:25 -0400

    added test docgen

--- Makefile
+++ Makefile
@@ -3,7 +3,9 @@
 
 test:
 	nvim --noplugin --headless -u scripts/minimal_init.vim -c "PlenaryBustedDirectory lua/tests/automated/ { minimal_init = './scripts/minimal_init.vim' }"
-	# nvim --headless -c "PlenaryBustedDirectory lua/tests/automated/"
 
 lint:
 	luacheck lua
+
+gendocs:
+	nvim --headless --noplugin -u scripts/minimal_init.vim -c "luafile ./scripts/gendocs.lua" -c 'qa'
---
+++ scripts/gendocs.lua
@@ -0,0 +1,33 @@
+local docgen = require('docgen')
+
+local docs = {}
+
+docs.test = function()
+  -- Filepaths that should generate docs
+  local input_files = {
+    "./lua/neuron.lua",
+    "./lua/neuron/config.lua",
+    "./lua/neuron/mappings.lua.lua",
+  }
+
+  -- Maybe sort them that depends what you want and need
+  table.sort(input_files, function(a, b)
+    return #a < #b
+  end)
+
+  -- Output file
+  local output_file = "./doc/neuron.txt"
+  local output_file_handle = io.open(output_file, "w")
+
+  for _, input_file in ipairs(input_files) do
+    docgen.write(input_file, output_file_handle)
+  end
+
+  output_file_handle:write(" vim:tw=78:ts=8:ft=help:norl:\n")
+  output_file_handle:close()
+  vim.cmd [[checktime]]
+end
+
+docs.test()
+
+return docs
