04ac8cca7ce92ac4592b6d5a9506934cf22537c1
Jean Cavallo <jean.cavallo@hotmail.fr>
2022-02-25 14:03:05 +0100

    Update to latest neovim / telescope API

--- lua/neuron/telescope/actions.lua
+++ lua/neuron/telescope/actions.lua
@@ -3,6 +3,7 @@ local neuron = require("neuron")
 local cmd = require("neuron/cmd")
 local utils = require("neuron/utils")
 local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
 
 local M = {}
 
@@ -10,7 +11,7 @@ function M.insert_maker(key)
   return function(prompt_bufnr)
     actions.close(prompt_bufnr)
 
-    local entry = actions.get_selected_entry()
+    local entry = action_state.get_selected_entry()
     api.nvim_put({entry[key]}, "c", true, true)
   end
 end
@@ -18,11 +19,11 @@ end
 function M.edit_or_insert(prompt_bufnr)
   actions.close(prompt_bufnr)
 
-  local entry = actions.get_selected_entry()
+  local entry = action_state.get_selected_entry()
   if entry ~= nil then
     vim.cmd("edit " .. entry.value)
   else
-    local current_line = actions.get_current_line() -- todo, need pr telescope for this
+    local current_line = action_state.get_current_line() -- todo, need pr telescope for this
     cmd.new_and_callback(neuron.config.neuron_dir, function(data)
       vim.cmd("edit " .. data)
       utils.start_insert_header()
@@ -35,14 +36,14 @@ end
 function M.insert(prompt_bufnr)
   actions.close(prompt_bufnr)
 
-  local entry = actions.get_selected_entry()
+  local entry = action_state.get_selected_entry()
   api.nvim_put({entry.id}, "c", true, true)
 end
 
 function M.new(prompt_bufnr)
   actions.close(prompt_bufnr)
 
-  vim.cmd("edit " .. actions.get_current_line() .. ".md")
+  vim.cmd("edit " .. action_state.get_current_line() .. ".md")
 end
 
 return M
